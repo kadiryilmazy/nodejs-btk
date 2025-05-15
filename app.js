63e88574b467b49a946a73be29645c4faa4fe092
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-15 19:31:58 +0300

    refactor: replace url module with fs module in app.js; rename index.html to index2.html; update README and remove unused code

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-const url = require("url");
+const fs = require("fs");
 
-const address = "http://localhost:8080/default?year=2023&month=10";
+// const files = fs.readdir("./", function () {
+//     if (err) {
+//         console.error("Error reading directory:", err);
+//         return;
+//     }
+//     console.log("Files in directory:", files);
+// });
 
-const parsedUrl = url.parse(address, true);
+// const data = fs.readFile("index.html", "utf8", function (err, data) {
+//     if (err) {
+//         console.error("Error reading file:", err);
+//         return;
+//     }
+//     console.log("File content:", data);
+// });
 
-console.log(parsedUrl);
+// fs.writeFile("index.html", "<h1>Hello World</h1>", function (err) {
+//     if (err) {
+//         console.error("Error writing file:", err);
+//         return;
+//     }
+//     console.log("File written successfully");
+// });
+
+// fs.appendFile("index.html", "<h1>Hello World</h1>", function (err) {
+//     if (err) {
+//         console.error("Error appending file:", err);
+//         return;
+//     }
+//     console.log("File appended successfully");
+// });
+
+fs.rename("index.html", "index2.html", function (err) {
+    if (err) {
+        console.error("Error renaming file:", err);
+        return;
+    }
+    console.log("File renamed successfully");
+});
--- readme.md
+++ readme.md
@@ -1,4 +1,1 @@
-const url = require("url");
-const address = "http://localhost:8080/default?year=2023&month=10";
-const parsedUrl = url.parse(address, true);
-console.log(parsedUrl);
+fs modülü ile klasör okuma yazma değiştirme işlemi.
