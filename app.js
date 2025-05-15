f03ccc0408329927a3aa18be64f0865529dfda7c
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-15 21:00:10 +0300

    feat: implement basic HTTP server in app.js; remove logger module

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const Logger = require("./logger");
-const logger = new Logger();
-logger.log("User logged in");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.write("Hello World");
+        res.end();
+    }
+
+    if (req.url === "/api/products") {
+        res.write("Product list");
+        res.end();
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
