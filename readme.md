df0f2060237acf5b79ca44caac51d4bd0c165f47
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-15 16:57:26 +0300

    refactor: clean up HTML and JavaScript files by removing unused code and updating console log statements

--- readme.md
+++ readme.md
@@ -1,19 +1,2 @@
-Tarayıcıda ki window a ek olarak node js de module.export yöntemiyle dışarıya açılıp, require methodu ile başka dosyalarda,
-ilgili tanımlamalar kullanılabilir.F
---SCRIPT A
-var firstName = "Kadir";
-var privateAge = 30;
-var log = function (name) {
-console.log("Hello " + name);
-};
-
-log("Kadir");
-
-//SCRIPT A EXPORT ISLEMI
-module.exports.log = log;
-module.exports.firstName = firstName;
-
---SCRIPT B
-const scriptA = require("./scriptA.js"); //scriptA yı ekle
-
-scriptA.log("script B says hello");
+console.log(**dirname); // Dosyanın bulunduğu dizin yolu
+console.log(**filename); // Dosyanın tam yolu
--- scriptA.js
+++ scriptA.js
@@ -1,26 +1,2 @@
-// var controllerA = (function () {
-//     //SCOPE FOR A
-
-//     // PRIVATE VARIABLES
-//     var age = 30;
-//     var firstName = "Kadir";
-
-//     //EXPORTED VARIABLES
-//     return { firstName };
-// })();
-
-// console.log(controllerA.firstName);
-// // console.log(controllerA.age); //undefined
-
-var firstName = "Kadir";
-var privateAge = 30;
-var log = function (name) {
-    console.log("Hello " + name);
-};
-
-log("Kadir");
-
-module.exports.log = log;
-module.exports.firstName = firstName;
-
-console.log(module);
+console.log(__dirname); // Dosyanın bulunduğu dizin yolu
+console.log(__filename); // Dosyanın tam yolu
