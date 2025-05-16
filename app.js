4bce0af9660bfa757aec26c125bb66e21245d880
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-16 15:36:32 +0300

    feat: implement file reading in HTTP server; add index.html for response

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.write("Hello World");
-        res.end();
-    }
+    // res.setHeader("Content-Type", "text/plain");
+    // res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Type", "text/html");
+    res.statusCode = 200;
+    res.statusMessage = "Ok";
 
-    if (req.url === "/api/products") {
-        res.write("Product list");
-        res.end();
-    }
+    // res.write("Hello World");
+
+    // res.write(
+    //     JSON.stringify({
+    //         message: "Hello World",
+    //         status: "success",
+    //         data: {
+    //             name: "John Doe",
+    //             age: 30,
+    //             city: "New York",
+    //         },
+    //     })
+    // );
+
+    // res.write("<html><head><title>My First Server</title></head><body><h1>Hello World</h1></body></html>");
+
+    fs.readFile("./index.html", (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.statusMessage = "Internal Server Error";
+            res.write("Error reading file");
+            return res.end();
+        } else {
+            res.setHeader("Content-Type", "text/html");
+            res.statusCode = 200;
+            res.statusMessage = "Ok";
+            res.end(data);
+        }
+    });
 });
 
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+server.listen(3000);
+console.log("Server is running on port 3000");
