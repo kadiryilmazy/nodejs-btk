15b64f36a2ac8a1ff45f787c7898cd0f10c1378c
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-17 20:59:00 +0300

    feat: add body-parser middleware; implement product addition form and handling

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
-
-app.use("/", (req, res, next) => {
-    console.log("Loglama yapıldı..");
-    next();
-});
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-    res.send("<h1>Add product page</h1>");
+    res.send(
+        `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Add a New Product</title>
+        </head>
+        <body>
+            <form action="product" method="POST">
+            <input type="text" name="productName" />
+            <input type="submit" value="Save Product" />
+            </form>
+        </body>
+        </html>
+        `
+    );
+});
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
 });
 
 app.use("/product-list", (req, res, next) => {
