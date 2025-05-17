73c481fddac453935c44c1e4e6465bb18e12ba45
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-18 00:18:26 +0300

    feat: add custom 404 error handling; provide navigation links for missing pages

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const userRoutes = require("./routes/user");
 app.use("/admin", adminRoutes);
 app.use(userRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send(
+        `<h1>Page Not Found</h1>
+        <a href="/admin/add-product">Add-Product</a><br/>
+        <a href="/">Home</a>`
+    );
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
