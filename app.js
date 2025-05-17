6a56f7eab7667fcfe12207c947afe7d2aca1c921
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-17 19:28:00 +0300

    feat: add middleware logging to Express server; enhance request handling

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use((req, res, next) => {
+    console.log("middleware 1 worked");
+    next();
 });
 
-app.get("/api/products", (req, res) => {
-    res.json({ message: "Hello from the API!" });
+app.use((req, res, next) => {
+    console.log("middleware 2 worked");
+    res.send("<h1>Hello from express</h1>");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("middleware 3 worked");
 });
 
 app.listen(3000, () => {
