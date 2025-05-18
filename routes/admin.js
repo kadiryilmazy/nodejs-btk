ef866942aebd0c51b982730556399ee867e24d2a
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-18 21:36:32 +0300

    feat: refactor admin routes; add product management functionality and update views

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,48 @@
 const express = require("express");
 const router = express.Router();
 
-const path = require("path");
+const products = [
+    {
+        name: "Samsung S8",
+        price: 500,
+        image: "1.jpg",
+        description: "Released in 2017 with a 5.8-inch display.",
+    },
+    {
+        name: "Samsung S9",
+        price: 600,
+        image: "2.jpg",
+        description: "Released in 2018 with a 5.8-inch display.",
+    },
+    {
+        name: "Samsung S10",
+        price: 700,
+        image: "3.jpg",
+        description: "Released in 2019 with a 6.1-inch display.",
+    },
+    {
+        name: "Samsung S20",
+        price: 800,
+        image: "4.jpg",
+        description: "Released in 2020 with a 6.2-inch display.",
+    },
+];
 
 router.get("/add-product", (req, res, next) => {
-    res.render("add-product", { title: "Add a New Product" });
+    res.render("add-product", {
+        title: "Add a New Product",
+    });
 });
 
 router.post("/add-product", (req, res, next) => {
+    products.push({
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        description: req.body.description,
+    });
     res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const path = require("path");
+const admin = require("./admin");
 
 router.get("/", (req, res, next) => {
-    const products = [
-        {
-            name: "Samsung S8",
-            price: 500,
-            image: "1.jpg",
-            description: "Released in 2017 with a 5.8-inch display.",
-        },
-        {
-            name: "Samsung S9",
-            price: 600,
-            image: "2.jpg",
-            description: "Released in 2018 with a 5.8-inch display.",
-        },
-        {
-            name: "Samsung S10",
-            price: 700,
-            image: "3.jpg",
-            description: "Released in 2019 with a 6.1-inch display.",
-        },
-        {
-            name: "Samsung S20",
-            price: 800,
-            image: "4.jpg",
-            description: "Released in 2020 with a 6.2-inch display.",
-        },
-    ];
-
-    res.render("index", { title: "Homepage", products: products });
+    res.render("index", { title: "Homepage", products: admin.products });
 });
 
 module.exports = router;
