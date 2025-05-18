e6e8b36cc16f142b0d817061c9a34ab33cee915a
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-18 18:42:05 +0300

    feat: migrate views to Pug; update routing and error handling for improved structure

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser");
 const path = require("path");
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-const adminRoutes = require("./routes/admin");
-const userRoutes = require("./routes/user");
 app.use("/admin", adminRoutes);
 app.use(userRoutes);
 
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
+app.use((req, res) => {
+    res.status(404).render("404");
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("listening on port 3000");
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
 
 const path = require("path");
+
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+    res.render("add-product");
 });
 
 router.post("/add-product", (req, res, next) => {
-    console.log(req.body);
     res.redirect("/");
-});
-router.get("/products", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 module.exports = router;
