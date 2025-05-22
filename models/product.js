7b1495ce1f8e63ce510a5a955d756cb07454302d
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-05-22 18:48:53 +0300

    feat: update product model and views; add unique IDs for products and update links in admin and create product views

--- models/product.js
+++ models/product.js
@@ -1,17 +1,20 @@
 const products = [
     {
+        id: "1",
         name: "Sample Product",
         price: 19.99,
         imageUrl: "1.jpg",
         description: "This is a sample product.",
     },
     {
+        id: "2",
         name: "Sample Product",
         price: 19.99,
         imageUrl: "1.jpg",
         description: "This is a sample product.",
     },
     {
+        id: "3",
         name: "Sample Product",
         price: 19.99,
         imageUrl: "1.jpg",
@@ -21,6 +24,7 @@ const products = [
 
 module.exports = class Product {
     constructor(name, price, imageUrl, description) {
+        this.id = Math.floor(Math.random() * 99999) + 1;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
