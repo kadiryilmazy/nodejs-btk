8d3eaefd94035a876660d7d97f69b49f504df72a
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-06-20 01:24:25 +0300

    feat: integrate MongoDB with Mongoose; update user model and database connection logic

--- models/user.js
+++ models/user.js
@@ -1,16 +1,50 @@
-const Sequelize = require("sequelize");
+const getDb = require("../utility/database").getdb;
+const mongodb = require("mongodb");
 
-const sequelize = require("../utility/database");
+class User {
+    constructor(name, email, id) {
+        this.name = name;
+        this.email = email;
+        this._id = id ? new mongodb.ObjectID(id) : null;
+    }
 
-const User = sequelize.define("user", {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING,
-});
+    save() {
+        const db = getDb();
+        return db
+            .collection("users")
+            .insertOne(this)
+            .then((result) => {
+                console.log("User saved:", result);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    static findById(userId) {
+        const db = getDb();
+        return db
+            .collection("users")
+            .findOne({ _id: new mongodb.ObjectID(userId) })
+            .then((user) => {
+                return user;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    static findByUserName(username) {
+        const db = getDb();
+        return db
+            .collection("users")
+            .findOne({ name: username })
+            .then((user) => {
+                return user;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+}
 
 module.exports = User;
--- utility/database.js
+++ utility/database.js
@@ -1,27 +1,34 @@
 require("dotenv").config();
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+
 let _db;
+
 const mongoConnect = (callback) => {
     const uri = process.env.MONGO_URI;
-    MongoClient.connect(uri)
 
+    MongoClient.connect(uri, {
+        useNewUrlParser: true, // URL'yi daha doğru analiz eder
+        useUnifiedTopology: true, // Yeni sunucu keşif ve izleme motoru
+    })
         .then((client) => {
-            console.log("connected");
             _db = client.db("node-app");
             callback();
         })
         .catch((err) => {
-            console.log(err);
+            console.error("MongoDB bağlantı hatası:", err);
             throw err;
         });
 };
+
 const getdb = () => {
     if (_db) {
         return _db;
     }
-    console.log("No database found!");
+    console.error("No database found! Bağlantı kurulmadı.");
+    return null;
 };
+
 module.exports = {
     mongoConnect,
     getdb,
